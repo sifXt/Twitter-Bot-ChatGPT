5bbe3b359c666d09dbb3389f818303c2e2573db2
sifXt <99073079+sifXt@users.noreply.github.com>
2023-06-08 21:32:12 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,11 @@
+# openai key
+key2 = 'xxx'
+
+# twitter developer keys
+api_key = 'xxx'
+api_key_secret = 'xxx'
+bearer_token = 'xxx'
+access_token = 'xxx'
+access_token_secret = 'xxx'
+client_id = 'xxx'
+client_secret = 'xxx'
